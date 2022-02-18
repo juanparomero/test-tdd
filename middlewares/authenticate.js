170713d8100e08788ed47c889563c3a177634552
JP <juanpablo.romero@idesie.com>
2022-02-18 19:21:55 +0100

    Revert "test authenticate"
    This reverts commit 7a46a0a4bf169e163373cabbcffc734555128da6.

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,7 +1,4 @@
 module.exports = (req, res, next) => {
-    const userId = req.header('user_id')
-    if (userId !== 1) {
-        return res.sendStatus(403)
-    }
+    req.header('user_id')
     next()
 }
