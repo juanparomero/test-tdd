7a46a0a4bf169e163373cabbcffc734555128da6
JP <juanpablo.romero@idesie.com>
2022-02-18 19:04:13 +0100

    test authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,4 +1,7 @@
 module.exports = (req, res, next) => {
-    req.header('user_id')
+    const userId = req.header('user_id')
+    if (userId !== 1) {
+        return res.sendStatus(403)
+    }
     next()
 }
